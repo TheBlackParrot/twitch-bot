50ba95c88faf1fd167233a4ca528a211c3819932
TheBlackParrot <theblackparrot0@gmail.com>
2025-12-14 17:34:21 -0600

    oops

--- test.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-async function fuck() {
-	//console.log(await axios.get('http://127.0.0.1:8880/api/player'));
-	await axios.post('http://127.0.0.1:8880/api/player', {
-		volume: -10
-	});
-}
-fuck();
