3ea8ac477f717fd918c71f508c487ec9d0b69d9a
TheBlackParrot <theblackparrot0@gmail.com>
2026-02-02 02:28:10 -0600

    prevent adding to the items thrown leaderboard if we're in reso (for now)

--- main.js
+++ main.js
@@ -1752,7 +1752,10 @@ chatClient.onRaid(async (channel, user, raidInfo, msg) => {
 		say(channel, '⚠️⚠️⚠️ THIS STREAM CONTAINS LOTS OF FLASHING AND POTENTIALLY STROBING LIGHTS. If you are sensitive to flashing lights I would advise switching the stream to audio-only mode or closing the stream. Viewer discretion is advised. ⚠️⚠️⚠️');
 	}
 
-	await updateLeaderboardValues(raiderInfo.id, "Items Thrown", raidInfo.viewerCount);
+	if(initialCategory != "Resonite") {
+		// for now
+		await updateLeaderboardValues(raiderInfo.id, "Items Thrown", raidInfo.viewerCount);
+	}
 });
 chatClient.onRaidCancel((channel, msg) => { 
 	say(channel, "wait nevermind...");
