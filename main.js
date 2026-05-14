09ddbebd2bf91f1610dbfc726677c522e8124f6f
TheBlackParrot <theblackparrot0@gmail.com>
2026-05-14 14:42:10 -0500

    ensure oOOo's regex only triggers on single use of emote

--- main.js
+++ main.js
@@ -1730,8 +1730,10 @@ commandList.addRegex(`(audio|music)\\s.*\\s(choppy|crackly|chopping|crackling|cr
 });
 
 // funny ghost friend
-commandList.addRegex(`^(oOOo)`, async(channel, args, msg, user) => {
+commandList.addRegex(`^(oOOo)$`, async(channel, args, msg, user) => {
 	global.remoteSound.broadcast("ghost");
+}, {
+	caseInsensitive: false
 });
 
 // ====== CHAT STUFF ======
